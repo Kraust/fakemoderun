9a7e581e9ff639c87649df51b85af7662b8200b5
Kraust <secretdragoon@gmail.com>
2024-09-05 18:50:55 -0400

    Updated to be generic

--- fakemoderun.go
+++ fakemoderun.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
 	"reflect"
+	"strconv"
 	"strings"
 	"syscall"
 )
 
 var (
-	f_cache = flag.Bool("cache", false, "Prefer Cache Cores")
-	f_freq  = flag.Bool("freq", false, "Prefer Frequency Cores")
+	f_cores = flag.String("cores", "", "List of cores to set the affinitize the process to")
 )
 
 func killProcess(cmd *exec.Cmd) {
@@ -22,68 +25,48 @@ func killProcess(cmd *exec.Cmd) {
 	cmd.Process.Kill()
 }
 
-func getMask7950X3D() (uintptr, uintptr, error) {
-
-	// Cores 1-8, 17-24
-	cache := 0
-	cache |= 1 << 0
-	cache |= 1 << 1
-	cache |= 1 << 2
-	cache |= 1 << 3
-	cache |= 1 << 4
-	cache |= 1 << 5
-	cache |= 1 << 6
-	cache |= 1 << 7
-	cache |= 1 << 16
-	cache |= 1 << 17
-	cache |= 1 << 18
-	cache |= 1 << 19
-	cache |= 1 << 20
-	cache |= 1 << 21
-	cache |= 1 << 22
-	cache |= 1 << 23
-
-	// Cores 9-16, 25-32
-	freq := 0
-	freq |= 1 << 8
-	freq |= 1 << 9
-	freq |= 1 << 10
-	freq |= 1 << 11
-	freq |= 1 << 12
-	freq |= 1 << 13
-	freq |= 1 << 14
-	freq |= 1 << 15
-	freq |= 1 << 24
-	freq |= 1 << 25
-	freq |= 1 << 26
-	freq |= 1 << 27
-	freq |= 1 << 28
-	freq |= 1 << 29
-	freq |= 1 << 30
-	freq |= 1 << 31
-
-	return uintptr(cache), uintptr(freq), nil
-}
-
-func getMask(useCache bool, useFreq bool) (uintptr, error) {
+func getMask(coreMask string) (uintptr, error) {
 	// HACK: Just Testing for now
 	var mask uintptr = 0
 
-	cacheMask, freqMask, err := getMask7950X3D()
-	if err != nil {
-		return mask, err
+	ok := false
+	coreRanges := strings.Split(coreMask, ",")
+
+	for _, coreRange := range coreRanges {
+		cores := strings.Split(coreRange, "-")
+		switch len(cores) {
+		case 1:
+			num, err := strconv.Atoi(cores[0])
+			if err != nil {
+				return mask, err
+			}
+			mask |= 1 << num
+			ok = true
+			break
+		case 2:
+			min, err := strconv.Atoi(cores[0])
+			if err != nil {
+				return mask, err
+			}
+
+			max, err := strconv.Atoi(cores[1])
+			if err != nil {
+				return mask, err
+			}
+
+			for idx := min; idx <= max; idx++ {
+				mask |= 1 << idx
+				ok = true
+			}
+			break
+		default:
+			return mask, fmt.Errorf("Failed to parse core range [%s]", cores)
+		}
 	}
 
-	if !useCache && !useFreq {
-		return 0xFFFFFFFF, nil
-	}
-
-	if useCache {
-		mask |= cacheMask
-	}
-
-	if useFreq {
-		mask |= freqMask
+	if !ok {
+		log.Printf("No cores specified. Allocating all cores.")
+		mask = 0xffffffff
 	}
 
 	return mask, nil
@@ -95,7 +78,9 @@ func main() {
 	name := strings.Join(flag.Args()[:1], " ")
 	args := strings.Join(flag.Args()[1:], " ")
 
-	mask, err := getMask(*f_cache, *f_freq)
+	log.Printf("Using core mask [%s]", *f_cores)
+
+	mask, err := getMask(*f_cores)
 	if err != nil {
 		log.Fatalf("Failed to get process mask: %s", err)
 	}
@@ -110,6 +95,15 @@ func main() {
 	}
 
 	defer killProcess(cmd)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func(cmd *exec.Cmd) {
+		<-sigChan
+		killProcess(cmd)
+		os.Exit(1)
+	}(cmd)
 
 	kernel32 := syscall.NewLazyDLL("kernel32.dll")
 	setProcessAffinityMask := kernel32.NewProc("SetProcessAffinityMask")
